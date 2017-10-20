40084f5572920763df099ce937a018107b6a798b
Andrey Galkin <andrey@futoin.org>
2017-10-21 01:33:12 +0300

    Added SMTP settings type

---
+++ types/smtp.pp
@@ -0,0 +1,15 @@
+#
+# Copyright 2017 (c) Andrey Galkin
+#
+
+
+type CfWeb::SMTP = Struct[{
+    Optional[host] => String[1],
+    Optional[port] => Integer[1, 65535],
+    Optional[start_tls] => Boolean,
+    Optional[auth_mode] => Enum['plain', 'login', 'cram_md5'],
+    Optional[user] => String[1],
+    Optional[password] => String[1],
+    Optional[from] => String[1],
+    Optional[reply_to] => String[1],
+}]
